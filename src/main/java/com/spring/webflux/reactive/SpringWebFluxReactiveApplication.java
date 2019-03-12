cd82899f50d71330ea2d8bd12d18b69293f4f4b0
Krishma <krishma.sood@gmail.com>
2019-03-12 22:20:31 +0530

    comment added

--- src/main/java/com/spring/webflux/reactive/SpringWebFluxReactiveApplication.java
+++ src/main/java/com/spring/webflux/reactive/SpringWebFluxReactiveApplication.java
@@ -3,6 +3,11 @@ package com.spring.webflux.reactive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//we have given nothing special to tell that this is a webflux application
+//we have included spring web flux starter maven dependency, 
+//so spring itself understands that this is a reactive application
+
 @SpringBootApplication
 public class SpringWebFluxReactiveApplication {
 
